919f452630701193f48c27ee9d9fed16a2de7e74
ValenPrograma <cardozovalentin2004@hotmail.com>
2023-02-03 15:35:34 -0300

    Listas tipo pila ya esta

---
+++ c# windows form .net/Problema de lista tipo pila/Problema de lista tipo pila/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Problema_de_lista_tipo_pila
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pila pila1;
+            pila1 = new Pila();
+            string cadena = textBox1.Text;
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                if (cadena.ElementAt(i) == '(' || cadena.ElementAt(i) == '[' || cadena.ElementAt(i) == '{')
+                {
+                    pila1.Insertar(cadena.ElementAt(i));
+                }
+                else
+                {
+                    if (cadena.ElementAt(i) == ')')
+                    {
+                        if (pila1.Extraer() != '(')
+                        {
+                            Text = "Incorrecto";
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (cadena.ElementAt(i) == ']')
+                        {
+                            if (pila1.Extraer() != '[')
+                            {
+                                Text = "Incorrecto";
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            if (cadena.ElementAt(i) == '}')
+                            {
+                                if (pila1.Extraer() != '{')
+                                {
+                                    Text = "Incorrecto";
+                                    return;
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+            if (pila1.Vacia())
+            {
+                Text = "Correcto";
+            }
+            else
+            {
+                Text = "Incorrecto";
+            }
+        }
+    }
+}
---
+++ c# windows form .net/Problema de lista tipo pila/Problema de lista tipo pila/Pila.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_de_lista_tipo_pila
+{
+    class Pila
+    {
+        class Nodo
+        {
+            public char simbolo;
+            public Nodo sig;
+        }
+        private Nodo raiz;
+
+        public Pila()
+        {
+            raiz = null;
+        }
+        
+        public void Insertar(char x)
+        {
+            Nodo nuevo;
+            nuevo = new Nodo();
+            nuevo.simbolo = x;
+            if(raiz == null)
+            {
+                nuevo.sig = null;
+                raiz = nuevo;
+            }
+            else
+            {
+                nuevo.sig = raiz;
+                raiz = nuevo;
+            }
+        }
+
+        public char Extraer()
+        {
+            if(raiz != null)
+            {
+                char simboloExtraido;
+                simboloExtraido = raiz.simbolo;
+                raiz = raiz.sig;
+                return simboloExtraido;
+            }
+            else
+            {
+                return char.MaxValue;
+            }
+        }
+
+        public bool Vacia()
+        {
+            if(raiz == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
